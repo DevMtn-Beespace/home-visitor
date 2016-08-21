bbe49524f0542033af50874a2612c4f7a93e2bf8
mlecke <mlecke@beespace.com>
2016-08-21 09:07:29 -0600

    admin teams

--- public/app/component/adminTeams/controller/adminTeamCtrl.js
+++ public/app/component/adminTeams/controller/adminTeamCtrl.js
@@ -1,18 +1,34 @@
 angular.module('app')
 .controller('adminTeamCtrl', function($scope){
 
+  $scope.members = ["name1","name2","name3","name4","name5","name6"];
+
   $scope.teams = [
-    {"team_name": "First Team",
+    {"id": 1,
+    "team_name": "First Team",
     "team_leader": "Michael",
     "team_second": "James"},
 
-    {"team_name": "Second Team",
+    {"id": 2,
+      "team_name": "Second Team",
     "team_leader": "Nancy",
     "team_second": "BooBoo"},
 
-    {"team_name": "Third Team",
+    {"id": 3,
+      "team_name": "Third Team",
     "team_leader": "Almaz",
     "team_second": "Diane"},
-  ];
+
+    {"id": 4,
+      "team_name": "Fourth Team",
+    "team_leader": "Jackson",
+    "team_second": "Jane"},
+
+    {"id": 5,
+      "team_name": "Fifth Team",
+    "team_leader": "Azeb",
+    "team_second": "Karima"},
+  ]
+
 
 });
