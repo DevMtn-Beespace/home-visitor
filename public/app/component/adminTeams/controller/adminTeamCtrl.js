f6e6ba0fb98a5c64ad60824ffb73ebad66dbcd31
mlecke <mlecke@beespace.com>
2016-08-20 21:55:24 -0600

    myteams cards made

--- public/app/component/adminTeams/controller/adminTeamCtrl.js
+++ public/app/component/adminTeams/controller/adminTeamCtrl.js
@@ -1,6 +1,18 @@
 angular.module('app')
 .controller('adminTeamCtrl', function($scope){
 
+  $scope.teams = [
+    {"team_name": "First Team",
+    "team_leader": "Michael",
+    "team_second": "James"},
 
+    {"team_name": "Second Team",
+    "team_leader": "Nancy",
+    "team_second": "BooBoo"},
+
+    {"team_name": "Third Team",
+    "team_leader": "Almaz",
+    "team_second": "Diane"},
+  ];
 
 });
--- public/app/component/myTeams/controller/myTeamCtrl.js
+++ public/app/component/myTeams/controller/myTeamCtrl.js
@@ -1,6 +1,23 @@
 angular.module('app')
 .controller('myTeamCtrl', function($scope){
 
+  $scope.teams = [
+    {"id": 1,
+      "team_name": "First Team",
+    "team_leader": "Michael",
+    "team_second": "James"},
 
+    {"id": 2,
+      "team_name": "Second Team",
+    "team_leader": "Nancy",
+    "team_second": "BooBoo"},
+
+    {"id": 3,
+      "team_name": "Third Team",
+    "team_leader": "Almaz",
+    "team_second": "Diane"},
+  ];
+
+  $scope.members = ["name1","name2","name3","name4","name5","name6"];
 
 });
